7db26d0a86b7ec2399c442e349289e71ca1671c2
James McCarthy <james.mccarthy@corp.clearcable.ca>
2021-02-13 23:13:45 -0500

    Added test routes

--- api/api.py
+++ api/api.py
@@ -1,8 +1,26 @@
 import time
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return jsonify(message='Hello from React Flask Planets'), 200
+
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    return jsonify(message = f"Hello, {name}. You are {age}.")
+
+
+@app.route('/url_vars/<string:name>/<int:age>')
+def url_vars(name: str, age: int):
+    return jsonify(message = f"Hello, {name}. You are {age}.")
